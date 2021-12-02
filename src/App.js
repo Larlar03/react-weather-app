1b8414dc862490ebbf58ed0b1387ac7dc3e85881
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2021-12-02 19:02:18 +0000

    Deleted files and reinstalled React

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>It works!</p>
+        <p>REACT</p>
       </header>
     </div>
   );
