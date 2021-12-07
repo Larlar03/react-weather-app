10f6b2c04a48c8165401af6c39fee6875a959baf
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2021-12-07 20:43:17 +0000

    Changed readme and added blank target for footer link

--- src/RepoLink.js
+++ src/RepoLink.js
@@ -5,7 +5,11 @@ import "./RepoLink.css";
 export default function RepoLink() {
   return (
     <div className="repo-link">
-      <a href="https://github.com/Larlar03/react-weather-app">
+      <a
+        href="https://github.com/Larlar03/react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>{" "}
       by Lana Gordon
